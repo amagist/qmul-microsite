584ae032afd68ab7704da289142791a755cc2285
amagist <amymagistris@hotmail.co.uk>
2018-02-23 12:23:04 +0000

    a whole bunch

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,6 +1,6 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 30) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
@@ -16,4 +16,13 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+});
+
+//changes QMUL logo from white to black when navbar collapses
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 150) {
+        $(".qmullogo").attr("src", "img/qmulblack.png");
+    } else {
+        $(".qmullogo").attr("src", "img/qmul.png");
+    }
 });
--- server.js
+++ server.js
@@ -4,3 +4,11 @@ connect().use(serveStatic(__dirname)).listen(8080, function () {
     console.log('Server running on 8080...');
 });
 //# sourceMappingURL=server.js.map
+
+var watson = require('watson-developer-cloud');
+
+var conversation = new watson.ConversationV1({
+  username: '2533be81-69d0-4bd8-8cd8-f47913eb8810',
+  password: 'qNZcHksmxsqh',
+  version_date: '2018-02-16'
+});
